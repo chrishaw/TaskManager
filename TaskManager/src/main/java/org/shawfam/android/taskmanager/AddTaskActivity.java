e7a195b5523d620deb67bbb7d82bb0f9b8b5b029
Chris Shaw <cshaw@freelinc.com>
2013-06-13 12:22:49 -0600

    Completed Week 1: Step 1.

---
+++ TaskManager/src/main/java/org/shawfam/android/taskmanager/AddTaskActivity.java
@@ -0,0 +1,14 @@
+package org.shawfam.android.taskmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Chris Shaw on 6/13/13.
+ */
+public class AddTaskActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_tasks);
+    }
+}
--- TaskManager/src/main/java/org/shawfam/android/taskmanager/ViewTasksActivity.java
+++ TaskManager/src/main/java/org/shawfam/android/taskmanager/ViewTasksActivity.java
@@ -1,23 +1,34 @@
 package org.shawfam.android.taskmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ViewTasksActivity extends Activity {
+
+    private Button addButton;
+    private TextView taskText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpViews();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.view_tasks, menu);
-        return true;
+    private void setUpViews() {
+        addButton = (Button)findViewById(R.id.add_button);
+        taskText = (TextView)findViewById(R.id.task_list_text);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ViewTasksActivity.this, AddTaskActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-    
+
 }
